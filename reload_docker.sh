4b775ce2575bd5c05aca670a76a77d7eb987f4b8
Otoniel Maya <otto94@gmail.com>
2019-04-18 11:38:41 -0500

    daemon.json

---
+++ reload_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo systemctl stop docker
+
+sudo cp daemon.json /etc/docker/
+
+sudo rsync -aqxP /var/lib/docker/ /home/zorbax/bin/Docker
+
+sudo systemctl start docker
+
+docker info
