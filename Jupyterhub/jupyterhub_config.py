f1f18a7832064993d8165895c2407a7993f37588
zorbax <otto94@gmail.com>
2021-03-10 23:43:06 +0100

    jupyter

---
+++ Jupyterhub/jupyterhub_config.py
@@ -0,0 +1,9 @@
+# jupyterhub_config.py
+c = get_config()
+
+# Change from JupyterHub to JupyterLab
+c.Spawner.default_url = '/lab'
+c.Spawner.debug = True
+
+# Administrators - set of users who can administer the Hub itself
+c.Authenticator.admin_users = {'atgenomics'}
